ec3780e86cbed10db46cb1126134960fec20dd99
razzalok <alokraj7184@gmail.com>
2023-09-12 12:07:28 +0530

    Drawer naviagtion added

--- App.js
+++ App.js
@@ -6,15 +6,20 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import {createDrawerNavigator} from '@react-navigation/drawer'
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
 
 const Stack =createNativeStackNavigator()
 const Drawer = createDrawerNavigator()
 
 function DrawerNavigator(){
   return(
-    <Drawer.Navigator>
+    <Drawer.Navigator screenOptions={{
+      headerStyle:{backgroundColor:'#351401'},
+      headerTintColor:'white',
+      sceneContainerStyle:{backgroundColor:'#3f2f25'}
+    }}>
       <Drawer.Screen name='category' component={CatogoryScreen}/>
-      <Drawer.Screen name='category' component={CatogoryScreen}/>
+      <Drawer.Screen name='favorites' component={FavoritesScreen}/>
     </Drawer.Navigator>
   )
 }
@@ -28,7 +33,7 @@ export default function App() {
         headerTintColor:'white',
         contentStyle:{backgroundColor:'#3f2f25'}
       }}>
-        <Stack.Screen name='Drawer' component={DrawerNavigator} options={{title:'All Categories'}}/>
+        <Stack.Screen name='Drawer' component={DrawerNavigator} options={{headerShown:false}}/>
         <Stack.Screen name='MealsOverView' component={MealsOverviewScreen}/>
         <Stack.Screen name='MealDetails' component={MealDetailScreen} options={{
           backgroundColor:'#3f2f25',
